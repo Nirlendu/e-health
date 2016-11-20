ae8ca75cbb728a2d433320e7cf0bdfa6f1652bb7
Nirlendu <nirlendu@gmail.com>
2016-11-20 19:56:05 +0530

    array_rotation init

---
+++ array_rotation/array_rotation.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#############
+#
+# Copyright - Nirlendu Saha
+#
+# author - nirlendu@gmail.com
+#
+#############
+
+def rotate(arr, n):
+	"""
+	Right side rotation of an array by n positions
+	"""
+	# Storing the length to avoid recalculation
+	arr_len = len(arr)
+	# Adjusting the value of n to account for Right side rotations and cases where n > length of the array
+    n = arr_len - (n % arr_len)
+    # Splitting the array into two parts and merging them again in reverse order (Second part + First part)
+    return arr[n:] + arr[:n]
