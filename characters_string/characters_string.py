28f6f2580a9bffe7475bd7df400211b981d80ecc
Nirlendu <nirlendu@gmail.com>
2016-11-21 01:11:15 +0530

    bugfix

--- characters_string/characters_string.py
+++ characters_string/characters_string.py
@@ -10,23 +10,56 @@
 
 from collections import OrderedDict
 
-def common_elements(first_arr, second_arr):
-	"""
-	Returns a list of common elements found in both lists
-	"""
-	return list(set(first_arr)-set(second_arr))
 
-def first_arr_unique_elements(first_arr, second_arr):
+def unique_elements_non_linear(first_arr, second_arr):
 	"""
-	Returns a list with unique elements in first list only -- Complexity -- O(N*N)
+	Returns a list with common elements in order of appearance in first list -- Complexity -- O(N*N)
 	"""
-	# List of unique elements in first list
-	first_arr_unique_elements = list(OrderedDict.fromkeys(first_arr))
-	# List of unique elements in second list
-	second_arr_unique_elements = list(OrderedDict.fromkeys(second_arr))
 	# List of common elements in both lists
-	common_elements_list = common_elements(first_arr_unique_elements, second_arr_unique_elements)
-	# Removing each common element from first list
-	for each_element in common_elements_list:
-		first_arr_unique_elements.remove(each_element)
-	return first_arr_unique_elements
+	common_elements_list = list(set(first_arr).intersection(set(second_arr)))
+
+	# Init the result array
+	result_arr = []
+
+	# Itirate over first array
+	for each_element in first_arr:
+		# If the element is present in common_elements_list, append to result_arr
+		if each_element in common_elements_list:
+			result_arr.append(each_element)
+
+	return result_arr
+
+
+def unique_elements_linear(first_arr, second_arr):
+	"""
+	Returns a list with common elements in order of appearance in first list -- Complexity -- O(N)
+	"""
+	reference_dict = OrderedDict()
+
+	# Itirating over each element of first array
+	for each_element in first_arr:
+		# If the element is repeatation, skip
+		if each_element in reference_dict:
+			continue
+		# If first time, then add to reference_dict and set the value to False
+		else:
+			reference_dict[each_element] = False
+
+	# Itirating over each element of second array
+	for each_element in second_arr:
+		# If the key is present, then it is common in both arrays
+		if each_element in reference_dict:
+			reference_dict[each_element] = True
+		# If the key is not present, then the element is not present in first array
+		else:
+			continue
+
+	# Init the result array
+	result_arr = []
+
+	for key,val in reference_dict.items():
+		# The keys which are present in both arrays
+		if val == True:
+			result_arr.append(key)
+
+	return result_arr
